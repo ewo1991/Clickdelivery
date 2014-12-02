4dadd9e6bc61fee4056cb9d762f555255605db62
ewo1991 <brayan_666_91@hotmail.com>
2014-12-02 13:01:40 -0500

    ya estoy cansado XD

--- Vistas/index/index.php
+++ Vistas/index/index.php
@@ -1,48 +1,18 @@
-        <div id="cuerpo">
-        	<table width="100%" height="493" border="0">
-  <tr>
-    <td width="49%">
-        <div id="imagen_fuc" class="unir"><img src="<?php echo BASE_URL?>Vistas/index/image/comofunciona.jpg" width="566" height="434" /></div>
-    </td>
-    <td width="51%">
-     <div id="busqueda" class="unir">
-            	<center><h1>&#191Que esperas?<br />
-				 Comienza a buscar</h1>
-            	</center>
-            	<table width="90%" border="0">
-            	  <tr align="center">
-            	    <td><h3>Por Restaurand</h3></td>
-            	    <td><h3>Por direcci&#243n </h3></td>
-          	    </tr>
-            	  <tr align="center">
-            	    <td>
-                        <div id="brest" class="busqueda" >
-                            <form action="" method="post">
-                                <input type="text" name="restaurand" /><br /><br />
-                                <input type="submit" value="Buscar" />
-                                </form>
-                        </div>
-                    </td>
-            	    <td>
-                    <div id="bciud" class="busqueda">
-                 		<form action="" method="post">
-                 			<select name="ciudad">
-                 				<option value="">----------------</option>
-                	 		</select><br /><br />
-                     		<input type="text" name="direccion" /><br /><br />
-                     		<input type="submit" value="Buscar" />
-                 		</form>
-                 </div>
-                    </td>
-          	    </tr>
-          	  </table>
-            	<p>&nbsp;</p>
-
-                 
-              
-          </div>
-    </td>
-  </tr>
-</table>
-
+<div>
+    <center>
+        <span>Busca por el nombre de Restaurante: </span>
+        <input type="text" name="buscar_restaurante" id="buscar_restaurante">
+        <input type="button" name="b_bus_rest" id="b_buscar_rest" value="Buscar">
+    </center>
 </div>
+<?php foreach (restaurante as $key=>$value){ ?>
+<div>
+    <div id="foto_index">
+    </div>
+    <div id="nombre_polleria">               
+    </div>
+    <div>
+        <input type="button" name="delivery" id="delivery" value="HAS TU DELIVERY">
+    </div>
+</div>
+<php } ?>
