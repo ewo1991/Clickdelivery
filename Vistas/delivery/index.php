fd8387829bd60a6afd7fc9290a717ea0ce9f532c
ewo1991 <brayan_666_91@hotmail.com>
2014-12-04 19:45:54 -0500

    subiendo

--- Vistas/delivery/index.php
+++ Vistas/delivery/index.php
@@ -1,6 +1,5 @@
-<div id="dialog_plato" title="Identificarse">
+<div id="dialog_plato" title="cantidad">
         <form id="frm_login">
-            <div id="nombre_plato"></div>
             <div>cantidad: <input type="text" name="cantidad" id="cantidad"></div>
         </form>
     </div>
@@ -8,15 +7,15 @@
     <?php foreach ($this->platos as $key=>$value){ ?>
         <div id="plato" class="class_plato">
             <div id="foto_plato"></div>
-            <div id="nombre_plato"><?php echo $value['nombre'] ?></div>
+            <div id="nombre_plato"><?php echo $value['nombre'] ?><input type="hidden" id="nombre_pl<?php echo $value['idPlato'] ?>" value="<?php echo $value['nombre'] ?>"></div>
             <div id="descripcion"><?php echo $value['descripcion'] ?></div>
-            <div id="precio"><?php echo $value['precio'] ?></div>
+            <div id="precio"><?php echo $value['precio'] ?><input type="hidden" id="precio_pl<?php echo $value['idPlato'] ?>" value="<?php echo $value['precio'] ?>"></div>
             <div id="boton_plato"><input type="button" name="b_plato" class="b_plato" id="b_plato" idplato="<?php echo $value['idPlato'] ?>" value="Camprar"></div>
         </div>
     <?php } ?>
 </div>
 
-<div id="carrito_compra_plato" class="cuerpo_plato">sfsdfasd
+<div id="carrito_compra_plato" class="cuerpo_plato">
     <form id="form_carrito">
         <table id="tablita">
             
--- Vistas/delivery/js/index.js
+++ Vistas/delivery/js/index.js
@@ -1,6 +1,12 @@
 $(function(){ 
+    var id_plato;
+    var nom_plato;
+    var prec_plato;
     $(".b_plato").click(function(){
-            $("#dialog_plato").dialog("open");
+        id_plato=$(this).attr('idplato');
+        nom_plato=$('#nombre_pl'+id_plato).val();
+        prec_plato=$('#precio_pl'+id_plato).val();
+        $("#dialog_plato").dialog("open");
         });
 
     $("#dialog_plato").dialog({
@@ -10,12 +16,24 @@ $(function(){
                 {
                     text: "Ok",
                     click: function() {
+                        canti=$('#cantidad').val();
+                        total=canti*prec_plato;
+                        $('#tablita').html(
+                            '<tr>\n\
+                                <td>'+nom_plato+'</td>\n\
+                                <td>'+prec_plato+'</td>\n\
+                                <td>'+canti+'</td>\n\
+                                <td>'+total+'</td>\n\
+                            </tr>'
+                        );
+                        $('#cantidad').val('');
                         $(this).dialog("close");
                     }
                 },
                 {
                     text: "Cancel",
                     click: function() {
+                        $('#cantidad').val('');
                         $(this).dialog("close");
                     }
                 }
