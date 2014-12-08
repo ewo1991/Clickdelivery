d3554fc980ec0cd9fc55b011770ae966130ab5c3
catitha <katherine_la13@hotmail.com>
2014-12-08 18:50:51 -0500

    estilos_cliente

--- Vistas/cliente/index.php
+++ Vistas/cliente/index.php
@@ -1,22 +1,39 @@
+<style type="text/css">
+
+.mis_datos{
+background-image: url('Vistas/index/image/datos.png');
+
+}
+.mis_pedidos{
+    background-image: url('Vistas/index/image/pedido.png');
+}
+.button{
+background: #4682B4;
+      border-radius: 10px;
+      color: #d7d7d7;
+     
+  }
+</style>
+
 <div id="cuerpo_cliente">
-    <div id="foto" clasa="linea"><center>
+    <div id="foto" class="linea"><center>
             <img width="240" height="270" src="/clickdelivery/uploads/<?php echo $_SESSION['nombre_empresa']  ?>">
             </center></div>
     <div id="tema"><h1>Mi cuenta</h1></div>
-	<div id="mis_datos" clasa="linea">
+	<div id="mis_datos" class="mis_datos">
             <div id="img_mis_dat_cliente">
                 
             </div>
             <center><div>
-            <input type="button" id="b_mis_datos" name="boton_m_d" value="Mis datos">
+            <input type="button" id="b_mis_datos" class="button" name="boton_m_d" value="Mis datos">
                 </div></center>
 	</div>
-    <div id="mis_pedidos" clasa="linea">
+    <div id="mis_pedidos" class="mis_pedidos">
             <div id="img_mis_pedi_cliente">
                 
             </div>
             <center><div>
-            <input type="button" id="b_mis_pedi" name="boton_m_p" value="Mis pedidos">
+            <input type="button" id="b_mis_pedi" class="button" name="boton_m_p" value="Mis pedidos">
                 </div></center>
 	</div>
 </div>
